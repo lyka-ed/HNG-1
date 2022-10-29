b0b400efdecf6dd16aa2262547abc4f972423823
Glory-Lyka Okposin <94672093+lyka-ed@users.noreply.github.com>
2022-10-29 12:48:49 +0100

    Add files via upload

---
+++ info/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view(['GET'])
+def bio(request):
+    if request.method == 'GET':
+        return Response({"slackusername": "lykaokpos",
+        "backend": True,
+        "age": 24,
+        "bio": "My name is Glory but I rather be called Lyka. I'm a big time foodie. Googling, YouTube and reading of articles from daily devs are what I enjoy doing effortlessly. I love playing chess even thou I ain't a pro yet."
+    })
+                        
